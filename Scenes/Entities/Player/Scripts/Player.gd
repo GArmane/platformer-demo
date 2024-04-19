ec9736e877995cbaaf87888cca3b3d8d9eb2ed19
Giovanni Armane <giovanniarmane@proton.me>
2024-04-18 21:39:47 -0300

    feat(project): part 1 completed

---
+++ Scenes/Entities/Player/Scripts/Player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite2d: Sprite2D = $Sprite2D
+@export var speed := 300.0
+@export var jump_velocity := -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: Variant = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+		sprite2d.flip_h = true if direction == -1 else false
+	else:
+		animation_player.play("Idle")
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
